e07d03f185123354c545532130edb913dd041f71
Ekaterina <chantea@mail.ru>
2025-10-16 19:59:06 +0300

    added new functional

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from masks import get_mask_account, get_card_number
+
+
+def mask_account_card(card_type_number: str) -> str:
+    """Функция, которая умеет обрабатывать информацию о картах и о счетах"""
+    card_info = card_type_number.rsplit(' ', 1)
+    if "Cчет" in card_type_number:
+        return f"{card_info[0]}{get_mask_account(card_info[1])}"
+    else:
+        return f"{card_info[0]}{get_card_number(card_info[1])}"
+
+def get_date(date_incorrect: str) -> str:
+    """Функция, которая умеет выдавать дату в формате ДД.ММ.ГГГГ"""
+    date_correct = f"{date_incorrect[8:10]}.{date_incorrect[5:7]}.{date_incorrect[0:4]}"
+    return date_correct
+
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 from masks import get_card_number, get_mask_account
+from widget import mask_account_card, get_date
 
 input_number_card = "4563975548726482"
 masked_card = get_card_number(input_number_card)
@@ -9,3 +10,12 @@ input_number_account = "25864741254787155555"
 masked_account = get_mask_account(input_number_account)
 print(f"введенный номер карты {input_number_account}")
 print(f"замаскированный номер карты {masked_account}")
+
+input_type_number_account = "Maestro 1596837868705199"
+masked_account_card = mask_account_card(input_type_number_account)
+print(f"Замаскированный аккаунт пользователя {masked_account_card}")
+
+input_datime = "2024-03-11T02:26:18.671407"
+date_correct = get_date(input_datime)
+print(date_correct)
+
